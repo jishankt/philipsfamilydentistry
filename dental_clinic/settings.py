6eeeb84ba89f048a4e73b8b5324b72044069d7bf
jishanktm <jishanktm214@gmail.com>
2026-02-27 01:00:23 +0530

    Fix middleware comma error

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
