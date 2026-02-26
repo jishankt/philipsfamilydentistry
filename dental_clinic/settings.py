80bfb09b3e08ab5869e1a9bdca6015ddf5de91cc
jishanktm <jishanktm214@gmail.com>
2026-02-27 01:11:23 +0530

    Fix middleware comma error

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "philipsfamilydentistry.onrender.com",
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
